a37246b787a000c7aca10ccecb11a801351696a7
ruthvika-29-11 <245122748011@mvsrec.edu.in>
2025-06-08 17:53:53 +0530

    Update Details.java

--- Details.java
+++ Details.java
@@ -5,9 +5,9 @@ class Details{
 public static void main(String[] args)
 {
     Scanner sc = new Scanner(System.in);
-    String id = sc.next();
-    String name = sc.next();
-    String Cl = sc.next();
+    String id = sc.nextLine();
+    String name = sc.nextLine();
+    String Cl = sc.nextLine();
 System.out.println("ID: "+id+" Name"+name+" Class"+Cl);
  
 }
