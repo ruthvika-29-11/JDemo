309305635df78f3ac214f115ae93a3cf0a5874cf
ruthvika-29-11 <245122748011@mvsrec.edu.in>
2025-06-08 17:48:12 +0530

    Add files via upload

---
+++ Details.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Details{
+
+public static void main(String[] args)
+{
+    Scanner sc = new Scanner(System.in);
+    int id = sc.nextInt();
+    String name = sc.next();
+    String Cl = sc.next();
+System.out.println("ID: "+id+" Name"+name+" Class"+cl);
+ 
+}
+}
